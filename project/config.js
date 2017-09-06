333257497758cc1583f5aee755cf9303e24e061e
Simon Cox <simon.cox@csiro.au>
2017-09-06 12:18:40 +1000

    Additional biblio entries re-order vocabulary description

--- project/config.js
+++ project/config.js
@@ -21,15 +21,38 @@ var respecConfig = {
   noRecTrack: true,
   overrideCopyright: "<p class='copyright'><a href='https://www.w3.org/Consortium/Legal/ipr-notice#Copyright'>Copyright</a> © 2017 <a href='http://www.csiro.au'><abbr title='Commonwealth Scientific and Industrial Organisation'>CSIRO</abbr></a> &amp; <a href='https://www.w3.org/'> <abbr title='World Wide Web Consortium'>W3C</abbr> </a><sup>®</sup> , <abbr title='World Wide Web Consortium'>W3C</abbr> <a href='https://www.w3.org/Consortium/Legal/ipr-notice#Legal_Disclaimer'>liability</a>, <a href='https://www.w3.org/Consortium/Legal/ipr-notice#W3C_Trademarks'>trademark</a> and <a href='https://www.w3.org/Consortium/Legal/copyright-documents'>document use</a> rules apply.</p>",
   localBiblio: {
-    "SCHEMA-ORG":{
+    "schema-org":{
       href:"http://schema.org/",
       title:"Schema.org"
     },
-    "GeoJSON":{
-      href:"http://geojson.org/geojson-spec.html",
-      title:"The GeoJSON Format Specification",
-      authors: ["Howard Butler", "Martin Daly", "Allan Doyle", "Sean Gillies", "Tim Schaub", "Christopher Schmidt"],
-      date: "16 June 2008"
+    "dbpedia-ont":{
+      href:"http://dbpedia.org/ontology/",
+      title:"DBPedia ontology"
+    },
+    "doap":{
+      href:"https://github.com/ewilderj/doap/wiki",
+      title:"Description of a Project",
+      authors: ["Edd Wilder-James"]
+    },
+    "frapo":{
+      href:"http://www.sparontologies.net/ontologies/frapo",
+      title:"FRAPO, the Funding, Research Administration and Projects Ontology",
+      authors: ["David Shotton"],
+      date: "04 September 2017"
+    },
+    "obo":{
+      href:"http://www.obofoundry.org/",
+      title:"The OBO Foundry"
+    },
+    "pdo":{
+      href:"http://vocab.deri.ie/pdo",
+      title:"Project Documents Ontology",
+      authors: ["Pradeep Varma"],
+      date: "09 July 2010"
+    },
+    "vivo-isf":{
+      href:"http://github.com/vivo-isf/vivo-isf",
+      title:"VIVO-ISF Data Standard"
     }
   },
   issueBase: "https://github.com/w3c/dxwg/issues/"
